ef8683d8f1dfd02d0653844f306f875869a26e70
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-04-01 15:14:24 -0400

    Setup K8S on all 4 machines

--- sheep-dog-cucumber-gen/scripts/forward-engineer.bat
+++ sheep-dog-cucumber-gen/scripts/forward-engineer.bat
@@ -1,4 +1,4 @@
 cd ..
 call mvn clean
-call mvn org.farhan:sheep-dog-svc-maven-plugin:uml-to-cucumber-guice -Dtags="cucumber-gen" -Dhost="dev.sheepdog.io"
+call mvn org.farhan:sheep-dog-svc-maven-plugin:uml-to-cucumber-guice -Dtags="cucumber-gen" -Dhost="qa.sheepdog.io"
 cd scripts
--- sheep-dog-grammar/scripts/forward-engineer.bat
+++ sheep-dog-grammar/scripts/forward-engineer.bat
@@ -1,3 +1,3 @@
 cd ..
-call mvn clean org.farhan:sheep-dog-svc-maven-plugin:uml-to-cucumber-guice -Dtags="grammar" -Dhost="dev.sheepdog.io"
+call mvn clean org.farhan:sheep-dog-svc-maven-plugin:uml-to-cucumber-guice -Dtags="grammar" -Dhost="qa.sheepdog.io"
 cd scripts 
